88ace8a71ecf35201169004629949b69f05472f0
Keshav <keshavkumar14092001@gmail.com>
2021-12-22 23:33:56 +0530

    Final Commit After Correcting

--- routes/web.js
+++ routes/web.js
@@ -1,20 +1,9 @@
-const authController = require('../app/http/controllers/authController');
-const homeController = require('../app/http/controllers/homeController');
+const mainController = require('../app/http/controllers/homeController');
 
 function initRoutes(app) {
 
-    app.get('/', homeController().index);
+    app.get('/', mainController().index);
 
-    app.get('/register', authController().register);
-
-    app.post('/register', authController().postRegister);
-
-    app.get('/login', authController().login);
-
-    app.post('/login', authController().postLogin);
-
-    app.post('/logout', authController().logout);
-    
 }
 
-module.exports = initRoutes;
+module.exports = initRoutes
--- server.js
+++ server.js
@@ -1,66 +1,33 @@
-require('dotenv').config();
-const express = require('express');
+const express = require('express')
 const app = express();
-const expressLayout = require('express-ejs-layouts');
-const path = require('path');
-const ejs = require('ejs');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const flash = require('express-flash');
 const http = require('http');
 const server = http.createServer(app);
+const expressLayout = require('express-ejs-layouts');
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-// Connectiong to Database:
-mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log('***DB Connected***');
-});
-
-// Configuring Passport.js:
-const passportInit = require('./app/config/passport');
-passportInit(passport);
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Configuring flash messaging:
-app.use(flash());
-
-// Declaring public folder:
+// Declaring Public Folder:
 app.use(express.static('public'));
 
-// Enabling express ability to read json files and other types of data:
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// Configuring Global Middleware:
-app.use((req, res, next) => {
-    res.locals.session = req.session
-    res.locals.user = req.user
-    next()
-});
-
-// Setting Template Engine:
+// Setting Up the template Engine:
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
 
 // Routes:
-require('./routes/web')(app);
+require('./routes/route')(app);
 
 // Showing 404 not found page:
 app.use((req, res) => {
     res.status(404).render();
 })
 
-// Listening at given Port:
+// Listening to the Server:
 server.listen(PORT, () => {
     console.log(`The Server is running at PORT ${PORT}`);
-});
+})
 
-// Socket.io Configuration:
+// Socket.io Connection:
 const io = require('socket.io')(server);
 
 io.on('connection', (socket) => {
